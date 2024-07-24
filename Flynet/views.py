451faa20ffb48ca8cc95b1edeb067e289f2bf9d5
Daniel Wesley C <danielwesleyc@Daniels-MacBook-Air.local>
2024-07-24 11:59:33 +0530

    eleventh commit

--- Flynet/views.py
+++ Flynet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 def home(request):
     return render(request, "Homepage.html")
@@ -16,6 +17,30 @@ def Blog_and_gallery(request):
     return render(request, "Blog_and_gallery.html")
 
 def contact(request):
+    if request.method == "POST":
+        f_name = request.POST.get("Fisrt name")
+        l_name = request.POST.get("Last name")
+        mobile_no = request.POST.get("Mobile Number")
+        email = request.POST.get("E-mail")
+        job_title = request.POST.get("Job Title")
+        subject = request.POST.get("Subject")
+        message = request.POST.get("Message")
+
+        data = {
+            'f_name': f_name,
+            'l_name': l_name,
+            'mobile_no': mobile_no,
+            'email': email,
+            'job_title': job_title,
+            'subject': subject,
+            'message': message
+        }
+        message ='''
+        New message: {}
+
+        From: {}
+        '''.format(data['message'],data['email'])
+        send_mail(data['subject'],message,'',['kishorekumar10436@gmail.com'])
     return render(request, "contact.html")
 
 def sub_services(request):
--- Flynet_website/settings.py
+++ Flynet_website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import socket
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smpt.gmail.com'
+EMAIL_HOST_USER = 'kishorekumar10436@gmail.com'
+EMAIL_HOST_PASSWORD = 'Kishore2#'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+
+socket.gethostbyname(socket.gethostname())
